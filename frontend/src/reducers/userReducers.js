e13eb5a7719b93070daa58ddf887e7b5cc0477f8
Sunny   Rajak <srajak892@gmail.com>
2020-10-02 00:44:55 +0530

    Update userReducers.js

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -97,18 +97,7 @@ export const userListReducer = (state = { users: [] }, action) => {
   }
 }
 
-export const userDeleteReducer = (state = {}, action) => {
-  switch (action.type) {
-    case USER_DELETE_REQUEST:
-      return { loading: true }
-    case USER_DELETE_SUCCESS:
-      return { loading: false, success: true }
-    case USER_DELETE_FAIL:
-      return { loading: false, error: action.payload }
-    default:
-      return state
-  }
-}
+
 
 export const userUpdateReducer = (state = { user: {} }, action) => {
   switch (action.type) {
@@ -122,6 +111,19 @@ export const userUpdateReducer = (state = { user: {} }, action) => {
       return {
         user: {},
       }
+    default:
+      return state
+  }
+}
+
+export const userDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_DELETE_REQUEST:
+      return { loading: true }
+    case USER_DELETE_SUCCESS:
+      return { loading: false, success: true }
+    case USER_DELETE_FAIL:
+      return { loading: false, error: action.payload }
     default:
       return state
   }
